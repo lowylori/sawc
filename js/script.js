a38b13b11f25934a3ad3f32ac35ba8f63e30f025
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-04-04 22:39:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ function responsiveNav(y) {
       x.className = "navbar";
     }
   }
+//Expanding buttons
+var acc = document.getElementsByClassName("ExpandingButton");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var Article = this.nextElementSibling;
+    if (Article.style.display === "block") {
+      Article.style.display = "none";
+    } else {
+      Article.style.display = "block";
+    }
+  });
+}
